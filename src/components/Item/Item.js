4f6858fc80631911567321752df92dcd4c52571d
camibec <cami8bec@gmail.com>
2023-01-05 22:03:13 -0300

    probando

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,11 +3,12 @@ import img from '../../golocan.jpg'
 
 const Item = (prod) => {
     return (
-        <div className="card">
-            <img src={img} alt="img" />
-            <div className="container">
-                <b>{prod.name}</b> 
-                <p>${prod.price}</p>
+        <div className="card" style={{ width: 18}}>
+            <img className="card-img-top" src={img} alt="img" />
+            <div className="card-body">
+                <h5 className="card-title">{prod.name}</h5>
+                <p className="card-text">${prod.price}</p>
+                <button class="btn btn-primary">Ver</button>
             </div>
         </div>
     )
